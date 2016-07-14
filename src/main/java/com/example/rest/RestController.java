7511d0003829edfda0e4eddc352e4b269d89837e
tkysk <tkysk-github@example.com>
2016-07-14 15:23:13 +0900

    update to use some other literals

--- src/main/java/com/example/rest/RestController.java
+++ src/main/java/com/example/rest/RestController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 /**
  * Created by saeki on 2016/07/13.
  */
 @Controller
 public class RestController {
 
-    String[] names = {"taro","ichiro","hanako","sachiko","goro"};
+    String[] names = {"taro", "ichiro", "hanako", "sachiko", "goro"};
     String[] mails = {"taro@example.com", "ichi@ex", "hana@mail", "sa@mail", "goro@goro.com"};
 
     @RequestMapping("/rest/{id}")
@@ -26,14 +28,27 @@ public class RestController {
     public String showData(@PathVariable int id, Model model) {
         try {
             model.addAttribute("object", new DataObject(id, names[id], mails[id]));
+        } catch (NullPointerException e) {
         }
-        catch(NullPointerException e){}
 
-        model.addAttribute("msg","No."+id+" Data");
+        model.addAttribute("msg", "No." + id + " Data");
 
         return "showdata";
     }
 
+    @RequestMapping("/web/num/{num}")
+    public String shoWholeData(@PathVariable int num, Model model) {
+        ArrayList<DataObject> data = new ArrayList<DataObject>();
+
+        for (int i = 0; (i < names.length) && (i < mails.length); i++)
+            data.add(new DataObject(i, names[i], mails[i]));
+
+        model.addAttribute("data", data);
+        model.addAttribute("genMsg", "'No.'+ ${num} +' Data'");
+        model.addAttribute("num", num);
+
+        return "showdata2";
+    }
 }
 
 
